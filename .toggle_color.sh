6b0ac33aca55e70ce5c72eb90aa79d10e40a2f1c
Brian Zhang <brianz@stripe.com>
2024-02-07 16:59:55 -0800

    Update .toggle_color.sh to work with toml

--- .toggle_color.sh
+++ .toggle_color.sh
@@ -5,42 +5,38 @@
 DEFAULT_LIGHT_SCHEME=solarized-light
 DEFAULT_DARK_SCHEME=oceanicnext
 FAVORITE_SCHEMES="gruvbox-dark-hard\nporple\nmateria\nonedark\nhopscotch\natelier-savanna-light\natelier-estuary-light"
+ALACRITTY_CONFIG=$HOME/.alacritty.toml
 
 all_schemes() {
-    grep "^colors__[a-zA-Z0-9_-]*__\(dark\|light\):$" $HOME/.alacritty.yml | gsed 's/colors__\([a-zA-Z0-9_-]*\)__\(light\|dark\):/\1/'
+    grep "# colors__[a-zA-Z0-9_-]*__\(dark\|light\).primary =" "$ALACRITTY_CONFIG" | gsed "s/# colors__\([a-zA-Z0-9_-]*\)__\(light\|dark\).primary =.*/\1/"
 }
 
 get_current_scheme() {
-    grep '^colors: # [a-zA-Z0-9_-]* (\(dark\|light\))' $HOME/.alacritty.yml | gsed 's/.*: # \([a-zA-Z0-9_-]*\) (.*/\1/'
+    grep "^# \[colors\] [a-zA-Z0-9_-]* (\(dark\|light\))" "$ALACRITTY_CONFIG" | gsed "s/# \[colors\] \([a-zA-Z0-9_-]*\) (.*/\1/"
 }
 
 get_current_mode() {
-    grep '^colors: # [a-zA-Z0-9_-]* (\(dark\|light\))' $HOME/.alacritty.yml | gsed 's/.*(\([a-z]*\))$/\1/'
+    grep "^# \[colors\] [a-zA-Z0-9_-]* (\(dark\|light\))" "$ALACRITTY_CONFIG" | gsed "s/.*(\([a-z]*\))$/\1/"
 }
 
-__disable_active_scheme() {
-    current_scheme=$(get_current_scheme)
-    current_mode=$(get_current_mode)
-    # echo "disabling active scheme $current_scheme..."
-    gsed -i "s/^colors: # ${current_scheme} (${current_mode})$/colors__${current_scheme}__${current_mode}:/" $HOME/.alacritty.yml
-    # echo "done"
-}
-
-__enable_target_scheme() {
-    # echo "switching to $1..."
-    gsed -i "s/^colors__\($1\)__\(light\|dark\):$/colors: # \1 (\2)/" $HOME/.alacritty.yml
-    # echo -e "done.\nhappy hacking!"
+get_target_mode() {
+    grep "^# colors__$1__\(dark\|light\).primary" "$ALACRITTY_CONFIG" | gsed "s/^# colors__$1__\(dark\|light\)\.primary.*/\1/"
 }
 
 use_scheme() {
-    __disable_active_scheme && __enable_target_scheme $1
+    current_scheme=$(get_current_scheme)
+    current_mode=$(get_current_mode)
+    target_scheme=$1
+    target_mode=$(get_target_mode "$target_scheme")
+    gsed -i "s/^# \[colors\] \(${current_scheme} (${current_mode})\)$/# [colors] ${target_scheme} (${target_mode})/" "$ALACRITTY_CONFIG"
+    gsed -i "s/^colors.\(bright\|cursor\|normal\|primary\) = \(.*\)$/# colors__${current_scheme}__${current_mode}.\1 = \2/" "$ALACRITTY_CONFIG"
+    gsed -i "s/^# colors__${target_scheme}__${target_mode}.\(bright\|cursor\|normal\|primary\) = \(.*\)$/colors.\1 = \2/" "$ALACRITTY_CONFIG"
 }
 
 
 if [ -z "$1" ]; then
-    scheme=$(get_current_scheme)
     mode=$(get_current_mode)
-    if [ $mode == 'dark' ]; then
+    if [ "$mode" == 'dark' ]; then
         target=$DEFAULT_LIGHT_SCHEME
     else
         target=$DEFAULT_DARK_SCHEME
@@ -58,13 +54,13 @@ else
     elif all_schemes | grep -q "^$1$"; then
         target=$1
     else
-        echo -e "Bad input. Valid schemes: [$(echo $valid_schemes | tr ' ' "\n")]"
+        echo -e "Bad input. Valid schemes: [$(echo "$valid_schemes" | tr ' ' "\n")]"
         exit 1
     fi
 fi
 
-use_scheme $target
+use_scheme "$target"
 echo "Enjoy ${target}"
 sleep 0.1
-touch $HOME/alacritty.yml
+touch "$ALACRITTY_CONFIG"
 exit
